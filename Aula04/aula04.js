cff38a2d59bc0c2f197c76fd411ac123ee5883be
Gustavo Dias <lima364551441@outlook.com>
2026-01-08 17:47:13 -0300

    Aula04 - Sintaxe - Tipos de dados

---
+++ Aula04/aula04.js
@@ -0,0 +1,18 @@
+//Tipos de dados
+console.log(5+5); // números
+console.log("5 + 5"); // texto
+console.log("5"); //''letra''
+//=========================
+//Declarar variáveis: var, let , const 
+//Não usar '-' para separar nome da variável. Usa-se "_" ou tudo junto.
+var a = 1;
+var b = 5;
+var c = a + b;
+console.log(c); //Vai mostrar 6
+//==========================
+//Operadores aritméticos: +, -, *, /
+//Operadores de atribuição: = 
+//Sensível á maiúsculo e minúsculo.
+var PESSOA = "Gustavo";
+var pessoa = "Dias";
+console.log(pessoa) // Vai mostrar o "Dias"
