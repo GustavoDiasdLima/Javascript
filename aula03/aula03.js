b9cb4e88ca8c41253a0f3f369d1fc71e9d1ef039
Gustavo Dias <lima364551441@outlook.com>
2026-01-08 15:46:47 -0300

    Aula03 - Declarações

---
+++ aula03/aula03.js
@@ -0,0 +1,7 @@
+//Instruções que podem ser compostas por funções, valores, operadores, expressões, comentários etc.
+// ( ; ) separa uma instrução de outra.
+let a,b,c; a=1; b=2; c=3;
+function minhafuncao() {
+    //passar as declarações dentro das chaves.
+    //Blocos de códigos.
+}
